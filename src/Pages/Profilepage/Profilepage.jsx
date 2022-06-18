0724a60b6ec413ed90f1b7c38e0e54afecea5c8e
geerma <geerm1997@gmail.com>
2022-06-18 16:33:23 -0600

    Profile

--- src/Pages/Profilepage/Profilepage.jsx
+++ src/Pages/Profilepage/Profilepage.jsx
@@ -2,14 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./profilepage.css";
 
 import { Header } from "../../Components";
-import profileImage  from "../../Assets/Profile/Profile.png"
-import chatImage from "../../Assets/Chat.png"
-import tomato from "../../Assets/tomato.png"
+import profileImage from "../../Assets/Profile/Profile.png";
+import profileImageLarge from "../../Assets/Profile/ProfileLarge.png";
+import chatImage from "../../Assets/Profile/Chat.png";
+import historyImage from "../../Assets/Profile/History.png";
+import exitImage from "../../Assets/Profile/Exit.png";
+import settingsImage from "../../Assets/Profile/Settings.png";
 
 const Profile = () => {
   return (
-    <div>
+    <div className="profile_menu">
       <h1>Profile</h1>
+
+      <div className="profile_menu_container">
+        <img src={profileImageLarge} alt="profileimg" />
+        <div className="profile_top_text">
+          <h2>Name</h2>
+          <h3>4.8 Stars</h3>
+        </div>
+        <div className="profile_bottom_text">
+          <p>Based in Location</p>
+        </div>
+      </div>
     </div>
   );
 };
@@ -25,7 +39,23 @@ const Chats = () => {
 const TransactionHistory = () => {
   return (
     <div>
-      <h1>TransactionHistory</h1>
+      <h1>Transaction History</h1>
+    </div>
+  );
+};
+
+const Exit = () => {
+  return (
+    <div>
+      <h1>Exit</h1>
+    </div>
+  );
+};
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
     </div>
   );
 };
@@ -40,18 +70,31 @@ const Profilepage = () => {
       </div>
       <div className="profile_box">
         <div className="profile_icons">
-          <button onClick={() => setProfileMenu("Profile")}><img src={profileImage} alt="profileimg" /></button>
-          <button onClick={() => setProfileMenu("Chats")}><img src={chatImage} alt="profileimg" /></button>
-          <button onClick={() => setProfileMenu("History")}>History</button>
-          <button onClick={() => setProfileMenu("Exit")}>Exit</button>
-          <button onClick={() => setProfileMenu("Settings")}>Settings</button>
-          <img src={tomato} alt="tomato" />
+          <button onClick={() => setProfileMenu("Profile")}>
+            <img src={profileImage} alt="profileimg" />
+          </button>
+          <button onClick={() => setProfileMenu("Chats")}>
+            <img src={chatImage} alt="profileimg" />
+          </button>
+          <button onClick={() => setProfileMenu("History")}>
+            <img src={historyImage} alt="historyimg" />
+          </button>
+          <button onClick={() => setProfileMenu("Exit")}>
+            <img src={exitImage} alt="exitimg" />
+          </button>
+          <button onClick={() => setProfileMenu("Settings")}>
+            <img src={settingsImage} alt="settingsimg" />
+          </button>
         </div>
         <div className="profile_info">
           {profileMenu === "Chats" ? (
             <Chats />
           ) : profileMenu === "History" ? (
             <TransactionHistory />
+          ) : profileMenu === "Exit" ? (
+            <Exit />
+          ) : profileMenu === "Settings" ? (
+            <Settings />
           ) : (
             <Profile />
           )}
