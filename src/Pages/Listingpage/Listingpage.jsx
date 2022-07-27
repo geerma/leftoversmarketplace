ab67f36f798924c7e6dd1fcc761c16bf31ee7d6f
geerma <geerm1997@gmail.com>
2022-07-26 20:57:02 -0600

    Create post

--- src/Pages/Listingpage/Listingpage.jsx
+++ src/Pages/Listingpage/Listingpage.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./listingpage.css";
 import { Header } from "../../Components";
 
 const Listingpage = () => {
+  const backend_URL = "https://leftoverbackend.herokuapp.com";
+
+  const [postInfo, setPostInfo] = useState({
+    userId: "Default User ID",
+    postId: "Default Post ID",
+    name: "",
+    description: "Default Description",
+    itemType: "Item Type",
+    price: "$5",
+    pictureURL: "Picture URL",
+  });
+
+  const handlePostSubmit = (e) => {
+    e.preventDefault();
+    console.log(postInfo);
+
+    fetch(`${backend_URL}/api/posts/uploadPost`, {
+      method: "POST",
+      body: JSON.stringify({
+        userId: postInfo.userId,
+        postId: postInfo.postId,
+        name: postInfo.name,
+        description: postInfo.description,
+        itemType: postInfo.itemType,
+        price: postInfo.price,
+        pictureURL: postInfo.pictureURL,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .then(alert("Your post has been created"))
+      .catch((error) => console.log(error));
+
+    window.location.reload(false);
+
+  };
+
   return (
     <div className="listing_container">
       <div>
@@ -11,22 +51,64 @@ const Listingpage = () => {
       <h1>List an Item</h1>
       <div className="listing_inputs">
         <form className="listing_inputs">
-          <label>Price</label>
-          <input />
-
-          <label>Expiration Date</label>
-          <input />
-
           <label>Name of Item</label>
-          <input />
+          <input
+            type=""
+            placeholder="Name"
+            onChange={(e) =>
+              setPostInfo((prevState) => ({
+                ...prevState,
+                name: e.target.value,
+              }))
+            }
+          />
 
           <label>Description</label>
-          <input />
+          <input
+            type=""
+            placeholder="Description"
+            onChange={(e) =>
+              setPostInfo((prevState) => ({
+                ...prevState,
+                description: e.target.value,
+              }))
+            }
+          />
+
+          <label>Item Type</label>
+          <input
+            type=""
+            placeholder="Item Type"
+            onChange={(e) =>
+              setPostInfo((prevState) => ({
+                ...prevState,
+                itemType: e.target.value,
+              }))
+            }
+          />
+
+          <label>Price</label>
+          <input
+            type="number"
+            placeholder="Price"
+            onChange={(e) =>
+              setPostInfo((prevState) => ({
+                ...prevState,
+                price: e.target.value,
+              }))
+            }
+          />
+
+          <label>Best By</label>
+          <input type="date" placeholder="Best By" />
+
           <label>Major Allergens</label>
-          <input />
+          <input type="" placeholder="Major Allergens" />
           <label>Upload a Picture</label>
           <input type="file" id="photo" name="photo" accept="image/*" />
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={(e) => handlePostSubmit(e)}>
+            Submit
+          </button>
         </form>
       </div>
     </div>
